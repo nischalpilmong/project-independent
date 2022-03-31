39e01adf76b507dab4fe6a2ad56212a59456a5f2
nischalpilmong <nischalrai04@gmail.com>
2022-03-31 14:44:31 +0200

    browser and os specific design

---
+++ responsive-website1/script.js
@@ -0,0 +1,36 @@
+//const body = document.querySelector('body');
+
+if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+    {
+        alert('Opera');
+    }
+    else if(navigator.userAgent.indexOf("Edg") != -1 )
+    {
+        alert('Edge');
+    }
+    else if(navigator.userAgent.indexOf("Chrome") != -1 )
+    {
+       document.body.classList.add('chrome');
+    }
+    else if(navigator.userAgent.indexOf("Safari") != -1)
+    {
+        alert('Safari');
+    }
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+    {
+         document.body.classList.add('Firefox');
+    }
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+    {
+      alert('IE'); 
+    }  
+    else 
+    {
+       alert('unknown');
+    }
+
+if(navigator.userAgent.indexOf("Windows") != -1){
+    document.body.classList.add('windows');
+} else if(navigator.userAgent.indexOf("Mac") != -1){
+    document.body.classList.add('Mac');
+}    
